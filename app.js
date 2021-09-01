1f2f775199c6459619c0c14d9be610eca0336d0c
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2021-09-01 22:49:55 +0530

    add all packages

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-// const ejs = require("ejs");
-const port = 3000;
-const path = require('path');
+const port = 3000
+const express = require("express")
+const bodyParser = require("body-parser")
+const ejs = require("ejs");
+const mongoose = require("mongoose")
+const passport = require("passport")
+const session = require("express-session")
+const passportLocalMongoose = require("passport-local-mongoose")
+const LocalStrategy = require('passport-local').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
-// app.use(bodyParser.urlencoded({extended: false}));
+
+const app = express()
 
 app.use(express.static("public"));
 
-// app.set("view engine", "ejs");
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+
 app.listen(port, () => {
-    console.log(`Application running on port ${port}.`);
-  });
+  console.log(`Application running on port ${port}.`);
+});
 
 // Set up home route
 
 app.route("/").get((req, res) => {
-    res.render('home');
-  })
+  res.render('home');
+})
