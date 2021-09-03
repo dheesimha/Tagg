12208e73a07afcb3c10fc47c09c72200b4b092d3
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2021-09-03 12:40:05 +0530

    Connect to atlas

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const port = 3000
 const express = require("express")
 const bodyParser = require("body-parser")
@@ -6,6 +7,7 @@ const mongoose = require("mongoose")
 const passport = require("passport")
 const session = require("express-session")
 const passportLocalMongoose = require("passport-local-mongoose")
+const _ = require("lodash");
 const LocalStrategy = require('passport-local').Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
@@ -20,11 +22,99 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(session({
+  secret: process.env.secret,
+  resave: false,
+  saveUninitialized: false,
+}))
+
+app.use(passport.initialize())
+
+app.use(passport.session())
 
 
-app.listen(port, () => {
-  console.log(`Application running on port ${port}.`);
+const uri = `mongodb+srv://${process.env.MONGODB_ADMIN}:${process.env.MONGODB_PASSWORD}@cluster0.h1d0c.mongodb.net/taggDB?retryWrites=true&w=majority`
+
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+  .then(() => {
+    console.log("Connected to the cloud");
+  })
+
+  .catch((err) => {
+    console.log(err);
+  })
+
+
+// mongoose.set("useCreateIndex", true); 
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+
+  },
+
+  password: {
+    type: String,
+
+  },
+
+
+
 });
+
+
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: "username"
+});
+
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+
+passport.use(User.createStrategy())
+
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.authenticate(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+
+
 
 // Set up home route
 
@@ -76,3 +166,8 @@ app.route("/tag")
   .get((req, res) => {
     res.render("tag")
   })
+
+
+app.listen(port, () => {
+  console.log(`Application running on port ${port}.`);
+});
